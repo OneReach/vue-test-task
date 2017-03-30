1c51d0e1f77b7d5208b3c3e79a549419562c230d
Petro Matviienko <matviienkopetro@gmail.com>
2017-03-30 03:23:23 +0300

    test task vue

--- src/components/item.vue
+++ src/components/item.vue
@@ -1,4 +1,5 @@
 <template>
+  <ui-checkbox v-model="itemtodo.complete" v-bind:label="itemtodo.name"></ui-checkbox><!-- replace <input> with <ui-checkbox> in tasks list -->
 </template>
 <script>
   export default {
