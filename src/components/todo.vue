7754ebd7f035b87e078fb49341de89bd1c4c5c18
Serge Kovbasiuk <kolbasovs@gmail.com>
2017-03-31 12:12:14 +0300

    5

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -69,21 +69,6 @@
           return {
             newTaskName : '',
             tasks : taskStorage.fetch()
-            // [
-            //     {name : 'create skeleton of todo', complete : true},
-            //     {name : 'add ability to add tasks', complete : true},
-            //     {name : 'clear task name after clicking "Add"', complete : true},
-            //     {name : 'put "Add" button in one line with input', complete : true},
-            //     {name : 'add new task by hitting Enter instead of clicking "Add"', complete : true},
-            //     {name : 'replace <input> with <ui-checkbox> in tasks list', complete : true},
-            //     {name : 'when task is complete cross it out', complete : true},
-            //     {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : true},
-            //     {name : 'don\'t allow to add empty tasks', complete : true},
-            //     {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : true},
-            //     {name : 'extract list item into a separate vue.js component', complete : true},
-            //     {name : 'persist tasks list in a local storage', complete : true},
-            //     {name : 'add animation on task completion', complete : false},
-            // ]
           }
         },
         watch: {
@@ -155,13 +140,12 @@
       }
       &-enter,
       &-leave-to {
+        transform: translateY(100%);
         opacity: 0;
-        transform: translateX(30px);
       }
 
       &-enter {
-        transform: translateX(-100%);
-        opacity: 0;
+        transform: translateY(-100%);
       }
     }
 </style>
