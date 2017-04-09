b64a6f07ee89d49248f9758dd3287bc701a00c70
elena <elenalistratova11@gmail.com>
2017-04-09 16:14:32 +0300

    split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="todo">
         <h1 class="title">Checklist</h1>
-        <ul class="tasks">
-            <li v-for="(task, index) in tasks" :class="{complete : task.complete}">
-                <todo-item :task="task" :taskId="index" v-on:changed="updateTask"></todo-item>
-            </li>
-        </ul>
+        <div class="list">
+            <ui-tabs type="text">
+                <ui-tab title="pending">
+                    <ul class="tasks">
+                        <li v-for="(task, index) in pendingTasks" :class="{complete : task.complete}">
+                            <todo-item :task="task" :taskId="index" v-on:changed="updateTask"></todo-item>
+                        </li>
+                    </ul>
+                </ui-tab>
+                <ui-tab title="complete">
+                    <ul class="tasks">
+                        <li v-for="(task, index) in completeTasks" :class="{complete : task.complete}">
+                            <todo-item :task="task" :taskId="index" v-on:changed="updateTask"></todo-item>
+                        </li>
+                    </ul>
+                </ui-tab>
+            </ui-tabs>
+        </div>
         <div class="form">
             <ui-textbox placeholder="e.g. 'read vue.js guide'" v-model="newTaskName" @keydown-enter="addTask"></ui-textbox>
             <ui-button color="primary" @click="addTask" icon="add">Add</ui-button>
@@ -27,13 +40,26 @@
                     {name : 'add new task by hitting Enter instead of clicking "Add"', complete : true},
                     {name : 'replace <input> with <ui-checkbox> in tasks list', complete : true},
                     {name : 'when task is complete cross it out', complete : true},
-                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
+                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : true},
                     {name : 'don\'t allow to add empty tasks', complete : true},
                     {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : true},
                     {name : 'extract list item into a separate vue.js component', complete : true},
                     {name : 'persist tasks list in a local storage', complete : false},
                     {name : 'add animation on task completion', complete : false},
                 ]
+            }
+        },
+
+        computed: {
+            pendingTasks: function () {
+                return this.tasks.filter(function (task) {
+                    return !task.complete;
+                });
+            },
+            completeTasks: function () {
+                return this.tasks.filter(function (task) {
+                    return task.complete;
+                });
             }
         },
 
@@ -63,6 +89,8 @@
         padding: 20px;
         border-radius: 5px;
         box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 15px;
+        max-width: 600px;
+        width: 100%;
 
         .title {
             margin-top: 0;
