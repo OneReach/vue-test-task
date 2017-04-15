3f2f9b99cd878966ad52d20847de19a6f4c9aea0
trueteller89 <trueteller89@gmail.com>
2017-04-15 22:40:46 +0300

    test tasks done

---
+++ src/components/task.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>
+        <label :class="{complete : task.complete}" >
+            <input class="task-checkbox" type="checkbox" v-model="task.complete" /> {{task.name}}
+        </label>
+    </li>
+</template>
+<script>
+export default {
+  name: 'task',
+  props: {task:Object}
+}
+</script>
+<style scoped lang="scss">
+.complete 
+{
+    text-decoration:line-through;
+}
+</style>
