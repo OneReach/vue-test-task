f74deea32864a05f602d49f7cf149ab92154c549
AndriiKr <1dun1@gmail.com>
2017-04-24 09:59:53 +0300

    Test-task complete

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,47 +1,49 @@
 <template>
     <div class="todo">
         <h1 class="title">Checklist</h1>
-        <ul class="tasks">
-            <li v-for="task in tasks" :class="{complete : task.complete}">
-                <label>
-                    <input type="checkbox" v-model="task.complete" />
-                    {{task.name}}
-                </label>
-            </li>
-        </ul>
-        <div>
-            <ui-textbox placeholder="e.g. 'read vue.js guide'" v-model="newTaskName"></ui-textbox>
+        <ui-tabs type="text" fullwidth="true">
+            <ui-tab title="pending">
+                <ul class="tasks" >
+                    <li v-for="task in tasks" :class="{complete : task.complete}" v-if="task.complete == false && task.name!=''">
+                        <transition name="fade">
+                            <ui-checkbox type="checkbox" v-model="task.complete" :label="task.name"/>
+                        </transition>
+                    </li>
+                </ul>
+            </ui-tab>
+            <ui-tab title="complete">
+                <ul class="tasks" >
+                    <li v-for="task in tasks" :class="{complete : task.complete}" v-if="task.complete && task.name!=''">
+                        <transition name="fade">
+                            <ui-checkbox type="checkbox" v-model="task.complete" :label="task.name"/>
+                        </transition>
+                    </li>
+                </ul>
+            </ui-tab>
+        </ui-tabs>
+        <form class="input">
+            <ui-textbox class='text_submit' name="submit" placeholder="e.g. 'read vue.js guide'" v-model="newTaskName" type="text"></ui-textbox>
             <ui-button color="primary" @click="addTask" icon="add">Add</ui-button>
-        </div>
+        </form>
     </div>
 </template>
 
 <script>
+    import list from './list.vue';
+
+    localStorage.setItem('tasks', JSON.stringify(list.tasks));
     export default {
         data () {
             return {
                 newTaskName : '',
-                tasks : [
-                    {name : 'create skeleton of todo', complete : true},
-                    {name : 'add ability to add tasks', complete : true},
-                    {name : 'clear task name after clicking "Add"', complete : false},
-                    {name : 'put "Add" button in one line with input', complete : false},
-                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : false},
-                    {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
-                    {name : 'when task is complete cross it out', complete : false},
-                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
-                    {name : 'don\'t allow to add empty tasks', complete : false},
-                    {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : false},
-                    {name : 'extract list item into a separate vue.js component', complete : false},
-                    {name : 'persist tasks list in a local storage', complete : false},
-                    {name : 'add animation on task completion', complete : false},
-                ]
+                tasks: JSON.parse(localStorage.getItem('tasks'))
             }
         },
 
         methods : {
             addTask () {
                 this.tasks.push({name : this.newTaskName, complete : false});
+                this.newTaskName = '';
             }
         }
     };
@@ -60,8 +62,24 @@
         }
 
         .tasks {
+            height:400px;
+            overflow: auto;
             list-style: none;
             padding: 0;
+            .complete  {
+                text-decoration:line-through;
+            }
+
+            li label .animate {
+                animation:bounce .4s;
+            }
+        }
+        .input {
+            display: flex;
+            justify-content: space-around;
+            .text_submit {
+                width: 80%;
+            }
         }
     }
 </style>
