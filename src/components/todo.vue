c93397c6ae03ba74b62b0630b346bb87392e3ce8
Serhii Love <serhii.love@gmail.com>
2018-11-03 20:11:06 +0200

    split tasks into 'pending' and 'complete' tabs

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,13 +1,29 @@
 <template>
     <div class="todo">
         <h1 class="title">Checklist</h1>
-        <ul class="tasks">
-            <li v-for="task in tasks" :class="{complete : task.complete}">
-                <ui-checkbox type="checkbox" v-model="task.complete">
-                    <span>{{task.name}}</span>
-                </ui-checkbox>
-            </li>
-        </ul>
+        
+        <ui-tabs fullwidth>
+            <ui-tab id="pending-tab" title="Pending">
+                <ul class="tasks">
+                    <li v-for="task in tasks" :class="{complete : task.complete}">
+                        <ui-checkbox type="checkbox" v-model="task.complete">
+                            <span>{{task.name}}</span>
+                        </ui-checkbox>
+                    </li>
+                </ul>
+            </ui-tab>
+
+            <ui-tab id="complete-tab" title="Complete">
+                <ul class="tasks">
+                    <li v-for="task in tasks" :class="{complete : task.complete}">
+                        <ui-checkbox type="checkbox" v-model="task.complete">
+                            <span>{{task.name}}</span>
+                        </ui-checkbox>
+                    </li>
+                </ul>
+            </ui-tab>
+        </ui-tabs>
+        
         <div class="form-wrap">
             <ui-textbox placeholder="e.g. 'read vue.js guide'" v-model="newTaskName" ></ui-textbox>
             <ui-button color="primary" @click="addTask" icon="add">Add</ui-button>
@@ -23,11 +39,11 @@
                 tasks : [
                     {name : 'create skeleton of todo', complete : true},
                     {name : 'add ability to add tasks', complete : true},
-                    {name : 'clear task name after clicking "Add"', complete : true},
-                    {name : 'put "Add" button in one line with input', complete : true},
-                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : true},
-                    {name : 'replace <input> with <ui-checkbox> in tasks list', complete : true},
-                    {name : 'when task is complete cross it out', complete : true},
+                    {name : 'clear task name after clicking "Add"', complete : false},
+                    {name : 'put "Add" button in one line with input', complete : false},
+                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : false},
+                    {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
+                    {name : 'when task is complete cross it out', complete : false},
                     {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
                     {name : 'don\'t allow to add empty tasks', complete : false},
                     {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : false},
@@ -57,7 +73,8 @@
 
 <style scoped lang="scss">
     .todo {
-        margin: auto;
+        margin: 100px auto auto;
+        width: 500px;
         background: #fff;
         padding: 20px;
         border-radius: 5px;
@@ -80,6 +97,18 @@
 
         li.complete span {
             text-decoration: line-through;
+        }
+
+        #pending-tab li.complete {
+            display: none;
+        }
+
+        #complete-tab li {
+            display: none;
+
+            &.complete {
+                display: block;
+            }
         }
         
     }
