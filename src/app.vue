4e6260fdefa1491d82eaf04dbc55cc06548871df
Maxim Konoval <frontendmax@gmail.com>
2019-10-10 11:41:18 +0300

    Finish all tasks for single component

--- src/app.vue
+++ src/app.vue
@@ -1,40 +1,40 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    export default {
-        name : 'app'
-    }
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style lang="scss">
-    html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-        background: #eee;
-    }
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background: #eee;
+  }
 
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        color: #2c3e50;
-        height: 100%;
-        display: flex;
-    }
+  #app {
+    display: flex;
+    height: 100%;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-    // keen ui reset
-    *,
-    *::before,
-    *::after {
-        box-sizing: border-box;
-    }
+  // keen ui reset
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
 
-    html {
-        font-size: 100%;
-    }
+  html {
+    font-size: 100%;
+  }
 </style>
--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,67 +1,193 @@
 <template>
-    <div class="todo">
-        <h1 class="title">Checklist</h1>
+  <div class="todo">
+    <h1 class="title">Checklist</h1>
+    <ui-tabs>
+      <ui-tab
+        :key="tab.title"
+        :selected="tab.title === 'Pending'"
+        :title="tab.title"
+        v-for="tab in tabs"
+        @deselect="switchTab(tab.title)"
+      >
         <ul class="tasks">
-            <li v-for="task in tasks" :class="{complete : task.complete}">
-                <label>
-                    <input type="checkbox" v-model="task.complete" />
-                    {{task.name}}
-                </label>
-            </li>
+          <li v-for="(task, index) in tasksFiltered" :key="index" :class="{complete : task.complete}">
+            <ui-checkbox v-model="task.complete" color="clear">{{task.name}}</ui-checkbox>
+          </li>
         </ul>
-        <div>
-            <ui-textbox placeholder="e.g. 'read vue.js guide'" v-model="newTaskName"></ui-textbox>
-            <ui-button color="primary" @click="addTask" icon="add">Add</ui-button>
-        </div>
+      </ui-tab>
+    </ui-tabs>
+    <div class="add-task-controls">
+      <ui-textbox placeholder="Add an item here" v-model="newTaskName" @keydown.enter="addTask"></ui-textbox>
+      <ui-button color="primary" @click="addTask">Add item</ui-button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                newTaskName : '',
-                tasks : [
-                    {name : 'create skeleton of todo', complete : true},
-                    {name : 'add ability to add tasks', complete : true},
-                    {name : 'clear task name after clicking "Add"', complete : false},
-                    {name : 'put "Add" button in one line with input', complete : false},
-                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : false},
-                    {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
-                    {name : 'when task is complete cross it out', complete : false},
-                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
-                    {name : 'don\'t allow to add empty tasks', complete : false},
-                    {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : false},
-                    {name : 'extract list item into a separate vue.js component', complete : false},
-                    {name : 'persist tasks list in a local storage', complete : false},
-                    {name : 'add animation on task completion', complete : false},
-                ]
-            }
-        },
+  export default {
+    data () {
+      return {
+        newTaskName: '',
+        activeTab: 'Pending',
+        tabs: [
+          {title: 'Pending'},
+          {title: 'Completed'}
+        ],
+        tasks: [
+          {name: 'create skeleton of todo', complete: true},
+          {name: 'add ability to add tasks', complete: true},
+          {name: 'clear task name after clicking "Add"', complete: true},
+          {name: 'put "Add" button in one line with input', complete: true},
+          {name: 'add new task by hitting Enter instead of clicking "Add"', complete: true},
+          {name: 'replace <input> with <ui-checkbox> in tasks list', complete: true},
+          {name: 'when task is complete cross it out', complete: true},
+          {name: 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete: true},
+          {name: 'don\'t allow to add empty tasks', complete: true},
+          {name: 'make list of tasks scrollable, if there\'re are a lot of tasks', complete: true},
+          {name: 'extract list item into a separate vue.js component', complete: false},
+          {name: 'persist tasks list in a local storage', complete: false},
+          {name: 'add animation on task completion', complete: false},
+          {name: 'check me please :)', complete: false}
+        ]
+      }
+    },
+    methods: {
+      addTask () {
+        if (!this.newTaskName.trim()) return
 
-        methods : {
-            addTask () {
-                this.tasks.push({name : this.newTaskName, complete : false});
-            }
-        }
-    };
+        this.tasks.push({name : this.newTaskName, complete : false})
+        this.newTaskName = ''
+      },
+      switchTab (tab) {
+        this.activeTab = tab === 'Pending'
+          ? 'Completed'
+          : 'Pending'
+      }
+    },
+
+    computed: {
+      tasksFiltered () {
+        return this.activeTab === 'Pending'
+          ? this.tasks.filter(task => !task.complete)
+          : this.tasks.filter(task => task.complete)
+      }
+    }
+  }
 </script>
 
-<style scoped lang="scss">
-    .todo {
-        margin: auto;
-        background: #fff;
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 15px;
+<style lang="scss">
+  .todo {
+    width: 400px;
+    margin: auto;
+    background: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 15px;
 
-        .title {
-            margin-top: 0;
-        }
-
-        .tasks {
-            list-style: none;
-            padding: 0;
-        }
+    .title {
+      margin-top: 0;
     }
+
+    .tasks {
+      margin: 0;
+      padding: 15px 0 10px;
+      list-style: none;
+
+      .complete {
+        text-decoration: line-through;
+      }
+    }
+
+    .add-task-controls {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    // keen ui reset
+    .ui-tabs {
+      position: relative;
+
+      &::before,
+      &::after {
+        content: '';
+        position: absolute;
+        right: 0;
+        left: 0;
+        z-index: 1;
+        pointer-events: none;
+        height: 25px;
+      }
+
+      &::before {
+        top: 55px;
+        background-image: linear-gradient(to top, rgba(255,255,255,.1), #fff);
+      }
+
+      &::after {
+        bottom: 0;
+        background-image: linear-gradient(to bottom, rgba(255,255,255,.1), #fff);
+      }
+
+      &__header {
+        margin-bottom: 20px;
+        background-color: #fff !important;
+      }
+
+      &__body {
+        overflow: auto;
+        height: 250px;
+        margin-bottom: 50px;
+        border: none;
+      }
+    }
+      
+
+    .ui-tab-header-item {
+      height: 2.25rem;
+      color: #ddd;
+      border-radius: 20px;
+      transition: background-color .15s;
+
+      + .ui-tab-header-item {
+        margin-left: 20px;
+      }
+
+      &::after {
+        display: none;
+      }
+
+      &.is-active {
+        background-color: #aaa;
+
+        .ui-tab-header-item__text {
+          color: #fff;
+        }
+      }
+
+      &__text {
+        text-transform: none;
+        color: #aaa;
+        transition: color .15s;
+      }
+    }
+
+    .ui-checkbox__checkmark::after {
+      border-bottom-color: #333 !important;
+      border-right-color: #333 !important;
+    }
+
+    .ui-textbox {
+      margin-bottom: 0;
+
+      &__input {
+        border-bottom-width: 0 !important;
+      }
+    }
+
+    .ui-button {
+      text-transform: none;
+      border-radius: 20px;
+    }
+  }
 </style>
